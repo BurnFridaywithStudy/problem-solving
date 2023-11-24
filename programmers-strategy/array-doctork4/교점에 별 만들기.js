e2b2367193bfad911c5ae670b79eaee62b926110
doctork4 <doctor.k1994@gmail.com>
2023-11-24 18:24:46 +0900

    김동률 : 교점에 별 만들기 retry

--- programmers-strategy/array-doctork4/교점에 별 만들기.js
+++ programmers-strategy/array-doctork4/교점에 별 만들기.js
@@ -1,37 +1,44 @@
 function solution(line) {
   const axisArray = [];
-  
-  for (let i = 0; i < line.length; i++) {
-      for (let j = i + 1; j < line.length; j++) {
-          const result = getXY(line[i], line[j]);
-          if (result) axisArray.push(result);
-      }
+  for (let i = 0; i < line.length-1; i++) {
+    for (let j = i + 1; j < line.length; j++) {
+      const result = getXY(line[i], line[j]);
+      if (result !== null && !axisArray.includes(result)) axisArray.push(result);
+    }
   }
-  
-  console.log(axisArray)
-  
+  const set = new Set(axisArray);
+  if (axisArray.length !== set.size) console.log("중복 있음");
   // 좌표배열을 돌면서 사각형을 만든다.
-  let maxX = axisArray[0][0];
-  let minX = axisArray[0][0];
-  let maxY = axisArray[0][1];
-  let minY = axisArray[0][1];
-  
+  let maxX = -Infinity;
+  let minX = Infinity;
+  let maxY = -Infinity;
+  let minY = Infinity;
+  // 최댓값, 최소값 구하기
   axisArray.forEach(axis => {
-      if (axis[0] > maxX) maxX = axis[0];
-      if (axis[0] < minX) minX = axis[0];
-      if (axis[1] > maxY) maxY = axis[1];
-      if (axis[1] < minY) minY = axis[1];
+    maxX = Math.max(maxX, axis[0]);
+    minX = Math.min(minX, axis[0]);
+    maxY = Math.max(maxY, axis[1]);
+    minY = Math.min(minY, axis[1]);
   });
   const width = maxX - minX + 1;
   const height = maxY - minY + 1;
-  
+
+  // 최댓값, 최솟값 바탕으로 사각형 생성
   const square = makeSquare(width, height);
 
   // 좌표배열을 돌면서 사각형에 별을 찍는다.
   axisArray.forEach(axis => {
-      insertStar(square, axis);
+    const y = maxY - axis[1]; // 이 부분을 햇갈려서 한참 해멨다...
+    const x = axis[0] - minX; // 상대좌표를 어떻게 구해야하는걸까...
+    square[y][x] = "*";
   });
-  return square;    
+  let answer = [];
+  square.forEach(arr => {
+    let line = "";
+    Array.from(arr, (item) => line += item);
+    answer.push(line);
+  });
+  return answer;
 }
 
 // Ax+ By + C = 0
@@ -45,53 +52,42 @@ function solution(line) {
 //  2-2. 1이 아니라면 공식을 통해 x, y를 도출해낸다. 
 //  null이 아니라면 별표 좌표 목록들에 저장한다. 
 
-//  3-1. 교점이 하나인 경우
-  // 별 하나 출력하면 끝
 //  3-2. 교점이 2개 이상인 경우
   // 좌표정보들 중 가장 상단, 하단, 좌측, 우측에 있는 별이 무엇인지 판별하여 사각형 넓이를 도출해낸다.
-
-  // 상 : y값이 제일 큰 값
-  // 하 : y값이 제일 작은 값 
-  // 좌 : x값이 제일 작은 값
-  // 우 : x값이 제일 큰 값
-
-  // 사각형 가로 = 우 - 좌
-  // 사각형 세로 = 상 - 하
 
 //  4. 사각형을 생성한다.
 function makeSquare (width, height) {
   let square = [];    
   for (let i = 0; i < height; i++){
-      const oneLine = ".".repeat(width).split('');
-      square.push(oneLine);
+    const oneLine = ".".repeat(width).split('');
+    square.push(oneLine);
   }
-  
   return square;
 }
 
-// 5. 생성한 사각형을 교점 좌표들의 값에 맞게 "*"로 수정한다.
-function insertStar(square, axis) {
-  const y = axis[1];
-  const x = axis[0];
-  // TODO : square에 맞는 새로운 x, y값을 변환해야한다. 
-  console.log(x, y)
-  square[y][x] = "*";
-}
-
 function getXY(a, b) {
+  const [A, B, E] = a;
+  const [C, D, F] = b;
   // a = [A, B, E] b = [C, D, F]
-  if (a[0] * b[1] === a[1] * b[0]) {
-      return null;
-  }
-  // BF - ED / AD - BC
-  const x = ((a[1] * b[2]) - (a[2] * b[1])) / ((a[0] * b[1]) - (a[1] * b[0]));
-  // EC - AF / AD - BC
-  const y = ((a[2] * b[0]) - (a[0] * b[2])) / ((a[0] * b[1]) - (a[1] * b[0]));
-  
+  // AD - BC = 0 인경우 (두 직선이 평행한 경우)
+  if (A * D - B * C === 0) return null;
+  // x좌표 구하기 : BF - ED / AD - BC
+  const x = (B * F - E * D) / (A * D - B * C);
+  // y좌표 구하기 : EC - AF / AD - BC
+  const y = (E * C  - A * F) / (A * D - B * C);
   // 정수인 경우에만 좌표를 리턴한다.
   if (Number.isInteger(x) && Number.isInteger(y)) return [x, y];
   
   return null;
 }
 
-// 체감 난이도 : 5
+let input = [[2, -1, 4], [-2, -1, 4], [0, -1, 1], [5, -8, -12], [5, 8, 12]];
+let result = solution(input);
+let expected = ["....*....", ".........", ".........", "*.......*", ".........", ".........", ".........", ".........", "*.......*"];
+
+console.log(result, expected);
+
+input = [[0, 1, -1], [1, 0, -1], [1, 0, 1]]
+result = solution(input);
+expected = ["*.*"]
+console.log(result, expected);
